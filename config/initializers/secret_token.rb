9708390351f18afc4ab272f79c441b4d096522bc
John_AmazonRemote <johndbrennan@gmail.com>
2012-12-22 15:43:27 +0000

    first commit jb john_amazon

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PropertySite::Application.config.secret_token = 'feaead18f0cf33236b656a92c8e13e47d15bc62901b4d2ec03521922b63a359dedcc20002d5f3c1e0134c20fc0867f099efa431458bfffa5120999bddbf217bc'
