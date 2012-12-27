5cfcfd9cece6f9366379971e6435b200fea2de61
John <johndbrennan@gmail.com>
2012-12-27 16:02:37 +0000

    .

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
   
   def results
 	
-	@property = Property.where("price >= :min_price AND price <= :max_price", :min_price => params[:min_price],  :max_price => params[:max_price]).first
+	@properties = Property.where("price >= :min_price AND price <= :max_price", :min_price => params[:min_price],  :max_price => params[:max_price])
 	
   end
   
