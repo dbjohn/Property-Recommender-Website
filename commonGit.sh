c668d9bc9da1753e7d8a9634d3071bc0ac50b960
john_new_laptop_lapp <johndbrennan@gmail.com>
2013-01-20 14:15:04 +0000

    .

--- commonGit.sh
+++ commonGit.sh
@@ -1,14 +1,1 @@
-[master 20143ea] .
- Committer: john <john@lapp.(none)>
-Your name and email address were configured automatically based
-on your username and hostname. Please check that they are accurate.
-You can suppress this message by setting them explicitly:
-
-    git config --global user.name "Your Name"
-    git config --global user.email you@example.com
-
-If the identity used for this commit is wrong, you can fix it with:
-
-    git commit --amend --author='Your Name <you@example.com>'
-
- 1 files changed, 1 insertions(+), 0 deletions(-)
+git add -A && git commit -m "."
